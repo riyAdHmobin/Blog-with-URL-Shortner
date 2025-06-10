2c949e44e5db5a883a5fb4eb2b6eb68d2b01cf57
riyadhm0bin <mobinul.riyadh@polygontech.xyz>
2025-06-10 22:18:12 +0600

    updated

--- app/resources/views/urlShortner/list.blade.php
+++ app/resources/views/urlShortner/list.blade.php
@@ -1,39 +1,59 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>All Shortened URLs</title>
-</head>
-<body>
-<h1>Shortened URLs</h1>
-<a href="{{ route('url.shortener') }}">← Back to Shortener</a>
+@extends('layouts.app')
 
-<table border="1" cellpadding="8" cellspacing="0">
-    <thead>
-    <tr>
-        <th>ID</th>
-        <th>Original URL</th>
-        <th>Short Code</th>
-        <th>Short URL</th>
-        <th>Created At</th>
-    </tr>
-    </thead>
-    <tbody>
-    @forelse($urls as $url)
-        <tr>
-            <td>{{ $url->id }}</td>
-            <td><a href="{{ $url->original_url }}" target="_blank">{{ $url->original_url }}</a></td>
-            <td>{{ $url->short_code }}</td>
-            <td><a href="{{ url($url->short_code) }}" target="_blank">{{ url($url->short_code) }}</a></td>
-            <td>{{ $url->created_at->format('Y-m-d H:i') }}</td>
-        </tr>
-    @empty
-        <tr>
-            <td colspan="5">No URLs found.</td>
-        </tr>
-    @endforelse
-    </tbody>
-</table>
+@section('content')
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Blogs</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Blogs</a></li>
+                        <li class="breadcrumb-item active">Blogs List</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
 
-{{ $urls->links() }} <!-- Pagination -->
-</body>
-</html>
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+            <h1>Shortened URLs</h1>
+            <a href="{{ route('url.shortener') }}">← Back to Shortener</a>
+
+            <table border="1" cellpadding="8" cellspacing="0">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Original URL</th>
+                    <th>Short Code</th>
+                    <th>Short URL</th>
+                    <th>Created At</th>
+                </tr>
+                </thead>
+                <tbody>
+                @forelse($urls as $url)
+                    <tr>
+                        <td>{{ $url->id }}</td>
+                        <td><a href="{{ $url->original_url }}" target="_blank">{{ $url->original_url }}</a></td>
+                        <td>{{ $url->short_code }}</td>
+                        <td><a href="{{ url($url->short_code) }}" target="_blank">{{ url($url->short_code) }}</a></td>
+                        <td>{{ $url->created_at->format('Y-m-d H:i') }}</td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="5">No URLs found.</td>
+                    </tr>
+                @endforelse
+                </tbody>
+            </table>
+
+            {{ $urls->links() }} <!-- Pagination -->
+            <!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </section>
+@endsection
