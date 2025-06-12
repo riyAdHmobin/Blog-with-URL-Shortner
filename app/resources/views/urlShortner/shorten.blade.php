648d9372d235d0f39a64157a0c0d245852869dfa
riyadhm0bin <mobinul.riyadh@polygontech.xyz>
2025-06-12 12:32:50 +0600

    form updated

--- app/resources/views/urlShortner/shorten.blade.php
+++ app/resources/views/urlShortner/shorten.blade.php
@@ -1,15 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Shorten a URL</h1>
-    @if(session('shortUrl'))
-        <p>Shortened URL: <a href="{{ session('shortUrl') }}" target="_blank">{{ session('shortUrl') }}</a></p>
-    @endif
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Shorten a URL</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Blogs</a></li>
+                        <li class="breadcrumb-item active">Blogs List</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <section class="content">
+        <div class="container-fluid">
+        @if(session('shortUrl'))
+            <p>Shortened URL: <a href="{{ session('shortUrl') }}" target="_blank">{{ session('shortUrl') }}</a></p>
+       @endif
 
-    <form method="POST" action="{{ route('shorten') }}">
-        @csrf
-        <input type="url" name="original_url" placeholder="Enter URL" required>
-        <button type="submit">Shorten</button>
-    </form>
-    <a href="{{ route('url.list') }}">View all shortened URLs</a>
+        <form method="POST" action="{{ route('shorten') }}">
+            @csrf
+            <input type="url" class="form-control" name="original_url" placeholder="Enter URL" required>
+            <button type="submit" class="btn btn-primary" style="margin-top: 10px;">Shorten</button>
+        </form>
+        <a href="{{ route('url.list') }}">View all shortened URLs</a>
+    </div>
 @endsection
